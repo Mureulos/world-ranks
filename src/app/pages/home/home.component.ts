64647a2b29ea4513176962a3daf46c765b11a9c5
mureulos <murilodesantana05@gmail.com>
2024-08-05 13:59:45 -0300

    First commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { FilterComponent } from './components/filter/filter.component';
+import { Component, OnInit } from '@angular/core';
+import { CountriesService } from '../../shared/services/countries.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FilterComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private countriesService: CountriesService){}
 
+  ngOnInit(): void {
+    this.getAllCountries()
+  }
+
+  getAllCountries() {
+    this.countriesService.getAllCountries();
+  }
 }
--- src/app/shared/services/countries.service.ts
+++ src/app/shared/services/countries.service.ts
@@ -8,15 +8,25 @@ import { CountryType } from '../interface/types';
   providedIn: 'root'
 })
 export class CountriesService {
-  private urlAllCountries: string = environment.urlAllCountries
-  private urlName: string = environment.urlCountryName
-  private urlCode: string = environment.urlCountryCode
-  private urlCurrency: string = environment.urlCountryCurrency
-  private urlLanguage: string = environment.urlCountryLanguage
-  private urlCapitalCity: string = environment.urlCountryCapitalCity
-  private urlRegion: string = environment.urlCountryRegion
-  private urlSubregion: string = environment.urlCountryRegion
-  private urlTranslation: string = environment.urlCountryTranslation
+  // private urlAllCountries: string = environment.urlAllCountries
+  // private urlName: string = environment.urlCountryName
+  // private urlCode: string = environment.urlCountryCode
+  // private urlCurrency: string = environment.urlCountryCurrency
+  // private urlLanguage: string = environment.urlCountryLanguage
+  // private urlCapitalCity: string = environment.urlCountryCapitalCity
+  // private urlRegion: string = environment.urlCountryRegion
+  // private urlSubregion: string = environment.urlCountryRegion
+  // private urlTranslation: string = environment.urlCountryTranslation
+  
+  private urlAllCountries: string = 'https://restcountries.com/v3.1/all'
+  private urlName: string = 'https://restcountries.com/v3.1/name/'
+  private urlCode: string = 'https://restcountries.com/v3.1/alpha/'
+  private urlCurrency: string = 'https://restcountries.com/v3.1/currency/'
+  private urlLanguage: string = 'https://restcountries.com/v3.1/lang/'
+  private urlCapitalCity: string = 'https://restcountries.com/v3.1/capital/'
+  private urlRegion: string = 'https://restcountries.com/v3.1/region/'
+  private urlSubregion: string = 'https://restcountries.com/v3.1/subregion/'
+  private urlTranslation: string = 'https://restcountries.com/v3.1/translation/'
 
   constructor(private httpClient: HttpClient) {}
 
@@ -24,31 +34,31 @@ export class CountriesService {
     return this.httpClient.get<CountryType[]>(this.urlAllCountries)
   }
 
-  getCountryByFullName(name: string): Observable<CountryType> {
-    return this.httpClient.get<CountryType>(`${this.urlName}${name}`)
-  }
+  // getCountryByFullName(name: string): Observable<CountryType> {
+  //   return this.httpClient.get<CountryType>(`${this.urlName}${name}`)
+  // }
 
-  getCountryByCode(code: string): Observable<CountryType> {
-    return this.httpClient.get<CountryType>(`${this.urlCode}${code}`)
-  }
+  // getCountryByCode(code: string): Observable<CountryType> {
+  //   return this.httpClient.get<CountryType>(`${this.urlCode}${code}`)
+  // }
 
-  getCountryByLanguage(language: string): Observable<CountryType[]> {
-    return this.httpClient.get<CountryType[]>(`${this.urlLanguage}${language}`)
-  }
+  // getCountryByLanguage(language: string): Observable<CountryType[]> {
+  //   return this.httpClient.get<CountryType[]>(`${this.urlLanguage}${language}`)
+  // }
   
-  getCountryByCapitalCity(city: string): Observable<CountryType> {
-    return this.httpClient.get<CountryType>(`${this.urlCapitalCity}${city}`)
-  }
+  // getCountryByCapitalCity(city: string): Observable<CountryType> {
+  //   return this.httpClient.get<CountryType>(`${this.urlCapitalCity}${city}`)
+  // }
 
-  getCountryByRegion(region: string): Observable<CountryType[]> {
-    return this.httpClient.get<CountryType[]>(`${this.urlRegion}${region}`)
-  }
+  // getCountryByRegion(region: string): Observable<CountryType[]> {
+  //   return this.httpClient.get<CountryType[]>(`${this.urlRegion}${region}`)
+  // }
 
-  getCountryBySubregion(subregion: string): Observable<CountryType[]> {
-    return this.httpClient.get<CountryType[]>(`${this.urlSubregion}${subregion}`)
-  }
+  // getCountryBySubregion(subregion: string): Observable<CountryType[]> {
+  //   return this.httpClient.get<CountryType[]>(`${this.urlSubregion}${subregion}`)
+  // }
 
-  getCountryByNameTranslation(translationName: string): Observable<CountryType> {
-    return this.httpClient.get<CountryType>(`${this.urlTranslation}${translationName}`)
-  }
+  // getCountryByNameTranslation(translationName: string): Observable<CountryType> {
+  //   return this.httpClient.get<CountryType>(`${this.urlTranslation}${translationName}`)
+  // }
 }
